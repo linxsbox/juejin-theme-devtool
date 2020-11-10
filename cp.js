0a28f4ccd1b5dddeebe508bd16689eda159262ea
Linxs <yunfax@outlook.com>
2020-11-10 20:42:23 +0800

    fix: 删除文字阴影让人从视觉上感觉不舒服的因素
    - 新增 p 站风格

--- cp.js
+++ cp.js
@@ -1,3 +1,4 @@
 const copyFile = require('./tools/copyFile.js');
 
 copyFile('cyanosis.scss', '../juejin-markdown-theme-cyanosis/');
+// copyFile('psite.scss', '../juejin-markdown-theme-cyanosis/');
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const run = require('./tools/server.js');
 
 // 参数：样式文件 ， 同步预览掘金文章 id
 run('cyanosis.scss', '6856803264025853965');
+// run('psite.scss', '6893360456530919437');
