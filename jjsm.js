a98bfa18d4322f7f61132e8a2f484bcdddc3f6c0
Lin.xs <yunfax@outlook.com>
2022-04-16 17:39:08 +0800

    feat: 新增标准切换样式逻辑
    - 切换按钮更新为 input:checkbox
    - 切换事件变更为 click -> checked
    - 新增深色模式所需class
    - 新增运行提示信息

--- jjsm.js
+++ jjsm.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
+const {version, description, author, bugs} = require('./package.json')
+
+console.log(`\x1b[1;32m${description} v${version}\x1b[0m`);
+console.log('作者', `林小帅（${author}）\n`);
+console.log('如有使用问题 Issues ->', `\x1b[34m${bugs.url}\x1b[0m`);
+console.log('深色模式为 juejin-theme-devtool 作者私有实现，非官方支持！');
+console.log('深色模式切换的 chrome 插件目前正在开发中……\n');
+
 const viewServer = require('./tools/server.js');
 viewServer.run(process.argv.slice(2));
